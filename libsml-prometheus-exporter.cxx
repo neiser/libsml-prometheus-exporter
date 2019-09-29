db274ac29d4fefb8848690aa6401a82342c25436
Andreas Neiser <andreas.neiser@gmail.com>
2019-09-29 19:35:37 +0200

    Build runs with libsml and prometheus-cpp

--- libsml-prometheus-exporter.cxx
+++ libsml-prometheus-exporter.cxx
@@ -19,188 +19,184 @@
 // You should have received a copy of the GNU General Public License
 // along with libSML.  If not, see <http://www.gnu.org/licenses/>.
 
-
-#include <iostream>
-#include <stdio.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <getopt.h>
 #include <ctype.h>
 #include <errno.h>
-#include <termios.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#include <string.h>
+#include <fcntl.h>
+#include <getopt.h>
+#include <iostream>
 #include <math.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sys/ioctl.h>
+#include <termios.h>
+#include <unistd.h>
 
 #include <sml/sml_file.h>
 #include <sml/sml_transport.h>
 #include <sml/sml_value.h>
 
+#include <tclap/CmdLine.h>
+
+#include <prometheus/exposer.h>
+#include <prometheus/gauge.h>
+#include <prometheus/registry.h>
+
 #include "unit.h"
 
-// globals
-int sflag = false; // flag to process only a single OBIS data stream
-int vflag = false; // verbose flag
+int serial_port_open(const char *device) {
+  int bits;
+  struct termios config;
+  memset(&config, 0, sizeof(config));
 
-
-int serial_port_open(const char* device) {
-    int bits;
-    struct termios config;
-    memset(&config, 0, sizeof(config));
-
-    if (!strcmp(device, "-"))
-        return 0; // read stdin when "-" is given for the device
+  if (!strcmp(device, "-"))
+    return 0; // read stdin when "-" is given for the device
 
 #ifdef O_NONBLOCK
-    int fd = open(device, O_RDWR | O_NOCTTY | O_NONBLOCK);
+  int fd = open(device, O_RDWR | O_NOCTTY | O_NONBLOCK);
 #else
-    int fd = open(device, O_RDWR | O_NOCTTY | O_NDELAY);
+  int fd = open(device, O_RDWR | O_NOCTTY | O_NDELAY);
 #endif
-    if (fd < 0) {
-        fprintf(stderr, "error: open(%s): %s\n", device, strerror(errno));
-        return -1;
-    }
+  if (fd < 0) {
+    fprintf(stderr, "error: open(%s): %s\n", device, strerror(errno));
+    return -1;
+  }
 
-    // set RTS
-    ioctl(fd, TIOCMGET, &bits);
-    bits |= TIOCM_RTS;
-    ioctl(fd, TIOCMSET, &bits);
+  // set RTS
+  ioctl(fd, TIOCMGET, &bits);
+  bits |= TIOCM_RTS;
+  ioctl(fd, TIOCMSET, &bits);
 
-    tcgetattr(fd, &config);
+  tcgetattr(fd, &config);
 
-    // set 8-N-1
-    config.c_iflag &= ~(IGNBRK | BRKINT | PARMRK | ISTRIP | INLCR | IGNCR
-            | ICRNL | IXON);
-    config.c_oflag &= ~OPOST;
-    config.c_lflag &= ~(ECHO | ECHONL | ICANON | ISIG | IEXTEN);
-    config.c_cflag &= ~(CSIZE | PARENB | PARODD | CSTOPB);
-    config.c_cflag |= CS8;
+  // set 8-N-1
+  config.c_iflag &=
+      ~(IGNBRK | BRKINT | PARMRK | ISTRIP | INLCR | IGNCR | ICRNL | IXON);
+  config.c_oflag &= ~OPOST;
+  config.c_lflag &= ~(ECHO | ECHONL | ICANON | ISIG | IEXTEN);
+  config.c_cflag &= ~(CSIZE | PARENB | PARODD | CSTOPB);
+  config.c_cflag |= CS8;
 
-    // set speed to 9600 baud
-    cfsetispeed(&config, B9600);
-    cfsetospeed(&config, B9600);
+  // set speed to 9600 baud
+  cfsetispeed(&config, B9600);
+  cfsetospeed(&config, B9600);
 
-    tcsetattr(fd, TCSANOW, &config);
-    return fd;
+  tcsetattr(fd, TCSANOW, &config);
+  return fd;
 }
 
 void transport_receiver(unsigned char *buffer, size_t buffer_len) {
-    int i;
-    // the buffer contains the whole message, with transport escape sequences.
-    // these escape sequences are stripped here.
-    sml_file *file = sml_file_parse(buffer + 8, buffer_len - 16);
-    // the sml file is parsed now
+  int i;
+  // the buffer contains the whole message, with transport escape sequences.
+  // these escape sequences are stripped here.
+  sml_file *file = sml_file_parse(buffer + 8, buffer_len - 16);
+  // the sml file is parsed now
 
-    // this prints some information about the file
-    if (vflag)
-        sml_file_print(file);
-
-    // read here some values ...
-    if (vflag)
-        printf("OBIS data\n");
-    for (i = 0; i < file->messages_len; i++) {
-        sml_message *message = file->messages[i];
-        if (*message->message_body->tag == SML_MESSAGE_GET_LIST_RESPONSE) {
-            sml_list *entry;
-            sml_get_list_response *body;
-            body = (sml_get_list_response *) message->message_body->data;
-            for (entry = body->val_list; entry != NULL; entry = entry->next) {
-                if (!entry->value) { // do not crash on null value
-                    fprintf(stderr, "Error in data stream. entry->value should not be NULL. Skipping this.\n");
-                    continue;
-                }
-                if (entry->value->type == SML_TYPE_OCTET_STRING) {
-                    char *str;
-                    printf("%d-%d:%d.%d.%d*%d#%s#\n",
-                        entry->obj_name->str[0], entry->obj_name->str[1],
-                        entry->obj_name->str[2], entry->obj_name->str[3],
-                        entry->obj_name->str[4], entry->obj_name->str[5],
-                        sml_value_to_strhex(entry->value, &str, true));
-                    free(str);
-                } else if (entry->value->type == SML_TYPE_BOOLEAN) {
-                    printf("%d-%d:%d.%d.%d*%d#%s#\n",
-                        entry->obj_name->str[0], entry->obj_name->str[1],
-                        entry->obj_name->str[2], entry->obj_name->str[3],
-                        entry->obj_name->str[4], entry->obj_name->str[5],
-                        entry->value->data.boolean ? "true" : "false");
-                } else if (((entry->value->type & SML_TYPE_FIELD) == SML_TYPE_INTEGER) ||
-                        ((entry->value->type & SML_TYPE_FIELD) == SML_TYPE_UNSIGNED)) {
-                    double value = sml_value_to_double(entry->value);
-                    int scaler = (entry->scaler) ? *entry->scaler : 0;
-                    int prec = -scaler;
-                    if (prec < 0)
-                        prec = 0;
-                    value = value * pow(10, scaler);
-                    printf("%d-%d:%d.%d.%d*%d#%.*f#",
-                        entry->obj_name->str[0], entry->obj_name->str[1],
-                        entry->obj_name->str[2], entry->obj_name->str[3],
-                        entry->obj_name->str[4], entry->obj_name->str[5], prec, value);
-                    const char *unit = NULL;
-                    if (entry->unit &&  // do not crash on null (unit is optional)
-                        (unit = dlms_get_unit((unsigned char) *entry->unit)) != NULL)
-                        printf("%s", unit);
-                    printf("\n");
-                    // flush the stdout puffer, that pipes work without waiting
-                    fflush(stdout);
-                }
-            }
-            if (sflag)
-                exit(0); // processed first message - exit
+  // read here some values ...
+  for (i = 0; i < file->messages_len; i++) {
+    sml_message *message = file->messages[i];
+    if (*message->message_body->tag == SML_MESSAGE_GET_LIST_RESPONSE) {
+      sml_list *entry;
+      sml_get_list_response *body;
+      body = (sml_get_list_response *)message->message_body->data;
+      for (entry = body->val_list; entry != NULL; entry = entry->next) {
+        if (!entry->value) { // do not crash on null value
+          fprintf(stderr, "Error in data stream. entry->value should not be "
+                          "NULL. Skipping this.\n");
+          continue;
         }
+        if (entry->value->type == SML_TYPE_OCTET_STRING) {
+          char *str;
+          printf("%d-%d:%d.%d.%d*%d#%s#\n", entry->obj_name->str[0],
+                 entry->obj_name->str[1], entry->obj_name->str[2],
+                 entry->obj_name->str[3], entry->obj_name->str[4],
+                 entry->obj_name->str[5],
+                 sml_value_to_strhex(entry->value, &str, true));
+          free(str);
+        } else if (entry->value->type == SML_TYPE_BOOLEAN) {
+          printf("%d-%d:%d.%d.%d*%d#%s#\n", entry->obj_name->str[0],
+                 entry->obj_name->str[1], entry->obj_name->str[2],
+                 entry->obj_name->str[3], entry->obj_name->str[4],
+                 entry->obj_name->str[5],
+                 entry->value->data.boolean ? "true" : "false");
+        } else if (((entry->value->type & SML_TYPE_FIELD) ==
+                    SML_TYPE_INTEGER) ||
+                   ((entry->value->type & SML_TYPE_FIELD) ==
+                    SML_TYPE_UNSIGNED)) {
+          double value = sml_value_to_double(entry->value);
+          int scaler = (entry->scaler) ? *entry->scaler : 0;
+          int prec = -scaler;
+          if (prec < 0)
+            prec = 0;
+          value = value * pow(10, scaler);
+          printf("%d-%d:%d.%d.%d*%d#%.*f#", entry->obj_name->str[0],
+                 entry->obj_name->str[1], entry->obj_name->str[2],
+                 entry->obj_name->str[3], entry->obj_name->str[4],
+                 entry->obj_name->str[5], prec, value);
+          const char *unit = NULL;
+          if (entry->unit && // do not crash on null (unit is optional)
+              (unit = dlms_get_unit((unsigned char)*entry->unit)) != NULL)
+            printf("%s", unit);
+          printf("\n");
+          // flush the stdout puffer, that pipes work without waiting
+          fflush(stdout);
+        }
+      }
     }
+  }
 
-    // free the malloc'd memory
-    sml_file_free(file);
+  // free the malloc'd memory
+  sml_file_free(file);
 }
 
 int main(int argc, char *argv[]) {
-    // this example assumes that a EDL21 meter sending SML messages via a
-    // serial device. Adjust as needed.
-    int c;
+  try {
+    TCLAP::CmdLine cmd("libsml-prometheus-exporter", ' ', "0.1");
 
-    while ((c = getopt(argc, argv, "+hsv")) != -1) {
-        switch (c) {
-        case 'h':
-            printf("usage: %s [-h] [-s] [-v] device\n", argv[0]);
-            printf("device - serial device of connected power meter e.g. /dev/cu.usbserial, or - for stdin\n");
-            printf("-h - help\n");
-            printf("-s - process only one OBIS data stream (single)\n");
-            printf("-v - verbose\n");
-            exit(0); // exit here
-            break;
-        case 's':
-            sflag = true;
-            break;
-        case 'v':
-            vflag = true;
-            break;
-        case '?':
-            // get a not specified switch, error message is printed by getopt()
-            printf("Use %s -h for help.\n", argv[0]);
-            exit(1); // exit here
-            break;
-        default:
-            break;
-        }
-    }
+    TCLAP::ValueArg<std::string> arg_device(
+        "d", "device", "Device to read from", false, "/dev/ttyUSB0", "string");
+    cmd.add(arg_device);
 
-    if (argc - optind != 1) {
-        printf("error: Arguments mismatch.\nUse %s -h for help.\n", argv[0]);
-        exit(1); // exit here
-    }
+    TCLAP::MultiArg<std::string> arg_metric(
+        "m", "metric", "Specify metric to OBIS ID", false, "int");
+    cmd.add(arg_metric);
+
+    cmd.parse(argc, argv);
+
+    prometheus::Exposer exposer{"127.0.0.1:8080"};
+
+    auto registry = std::make_shared<prometheus::Registry>();
+
+    // add a new counter family to the registry (families combine values with
+    // the same name, but distinct label dimensions)
+    auto &gauge_family = prometheus::BuildGauge()
+                             .Name("time_running_seconds_total")
+                             .Help("How many seconds is this server running?")
+                             .Register(*registry);
+
+    auto &gauge = gauge_family.Add({});
+
+    exposer.RegisterCollectable(registry);
 
     // open serial port
-    int fd = serial_port_open(argv[optind]);
+    int fd = serial_port_open(arg_device.getValue().c_str());
     if (fd < 0) {
-        // error message is printed by serial_port_open()
-        exit(1);
+      // error message is printed by serial_port_open()
+      exit(1);
     }
+
+    gauge.Set(42);
 
     // listen on the serial device, this call is blocking.
     sml_transport_listen(fd, &transport_receiver);
     close(fd);
 
     return 0;
+
+  } catch (TCLAP::ArgException &e) // catch any exceptions
+  {
+    std::cerr << "error: " << e.error() << " for arg " << e.argId()
+              << std::endl;
+  }
 }
--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-umockdev-run -d mt681.umockdev -s /dev/ttyUSB0=ttyUSB0.script -- ./build/libsml-prometheus-exporter /dev/ttyUSB0
+umockdev-run -d mt681.umockdev -s /dev/ttyUSB0=ttyUSB0.script -- ./build/libsml-prometheus-exporter -d /dev/ttyUSB0
